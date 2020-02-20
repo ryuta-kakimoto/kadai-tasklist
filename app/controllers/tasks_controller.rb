dc19a44836aacb81fd23ec40c4a8fb2aaa5e1034
ryuta.kakimoto <06gwfz98@gmail.com>
2020-02-20 13:49:13 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,6 +51,10 @@ class TasksController < ApplicationController
 
   private
 
+　def set_task
+　    @task = Task.find(params[:id])
+　end
+
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
